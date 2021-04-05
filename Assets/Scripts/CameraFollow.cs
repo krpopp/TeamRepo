a80a27d5c22e7d065bf1d1e0c3499284e1b83d0b
Karina <karina.popp@nyu.edu>
2021-04-05 08:24:42 -0400

    init commit

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public Transform followTransform;
+    public BoxCollider2D worldBounds;
+    float xMin, xMax, yMin, yMax;
+    float camY, camX;
+
+    //holds the size of our vertical size of our camera
+    float camSize;
+    //holds the horizontal size of our camera
+    float camRatio;
+
+    Camera mainCam;
+    //staggered "smooth" position
+    Vector3 smoothPos;
+    //how quickly we should follow the player
+    public float smoothRate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        xMin = worldBounds.bounds.min.x;
+        xMax = worldBounds.bounds.max.x;
+        yMin = worldBounds.bounds.min.y;
+        yMax = worldBounds.bounds.max.y;
+        mainCam = gameObject.GetComponent<Camera>();
+        
+        //find our sizes and do some math to related them 
+        camSize = mainCam.orthographicSize;
+        camRatio = (xMax + camSize) / 8.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate() {
+        //clamp means we're limited our camera to an area
+        //offset them by half the size of our camera
+        camY = Mathf.Clamp(followTransform.position.y, yMin + camSize, yMax - camSize);
+        camX = Mathf.Clamp(followTransform.position.x, xMin + camRatio, xMax - camRatio);
+        
+        //lerp is liner interpolation
+        //go to a new position, from the current position at a set rate
+        smoothPos = Vector3.Lerp(this.transform.position, new Vector3(camX, camY, this.transform.position.z), smoothRate);
+        //go to the smoothed position
+        this.transform.position = smoothPos; 
+    }
+
+}
---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+
+    public float speed;
+    public float jumpHeight;
+    public float gravityMultiplier;
+
+    public Sprite jumpSprite;
+    public Sprite walkSprite;
+
+    Rigidbody2D myBody;
+    BoxCollider2D myCollider;
+    SpriteRenderer myRenderer;
+
+    float moveDir;
+    bool onFloor = true;
+    public static bool faceRight = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myBody = gameObject.GetComponent<Rigidbody2D>();
+        myCollider = gameObject.GetComponent<BoxCollider2D>();
+        myRenderer = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void FixedUpdate() {
+        if(onFloor && myBody.velocity.y > 0){
+            onFloor = false;
+        }
+        CheckKeys();
+        HandleMovement();
+        JumpPhysics();
+    }
+
+    void CheckKeys(){
+        if(Input.GetKey(KeyCode.D)){
+            moveDir = 1;
+            faceRight = true;
+            myRenderer.flipX = false;
+        } else if(Input.GetKey(KeyCode.A)){
+            moveDir = -1;
+            faceRight = false;
+            myRenderer.flipX = true;
+        } else{
+            moveDir = 0;
+        }
+        if(Input.GetKey(KeyCode.W) && onFloor){
+            myRenderer.sprite = jumpSprite;
+            myBody.velocity = new Vector3 (myBody.velocity.x, jumpHeight);
+        }else if(!Input.GetKey(KeyCode.W) && !onFloor){
+            myBody.velocity += Vector2.up * Physics.gravity.y * (jumpHeight - 1f) * Time.deltaTime;
+        }
+    }
+
+    void JumpPhysics(){
+        if(myBody.velocity.y < 0){
+            myBody.velocity += Vector2.up * Physics.gravity.y * (gravityMultiplier - 1f) * Time.deltaTime;
+        }
+    }
+
+    void HandleMovement(){
+        myBody.velocity = new Vector3(moveDir * speed, myBody.velocity.y);
+    }
+
+    void OnCollisionEnter2D(Collision2D other){
+        if(other.gameObject.tag == "Floor"){
+            myRenderer.sprite = walkSprite;
+            onFloor = true;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other){
+        if(other.gameObject.tag == "Enemy"){
+            Destroy(gameObject);
+        }
+    }
+}
