6a4d0da3517d205ef3a1b1e186879142d63f3fb5
MHOSS <hhoossppee@gmail.com>
2021-04-12 09:47:27 -0400

    Added comment to camera script

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
+    // Here's a change I made for the Camera Branch!
+    
     //this is my comment actually
     //here's another comments
     //ahhhh
