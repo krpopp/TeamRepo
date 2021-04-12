40f6ac9ebbeb2e36ef21c61f7ff974d8c1d76ec4
Jordan Grayson <jtg400@nyu.edu>
2021-04-12 09:30:02 -0400

    Added the core score system

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    private float score;
+
+  
+
+    public void SetScore(float newScore) 
+    {
+        score = newScore;
+    }
+
+    public float GetScore() 
+    {
+        return score;
+    }
+
+}
